99b3f0ef1533162b07283c7096ccd2e07691fc4c
Nante <nantemino15@gmail.com>
2023-07-10 14:49:45 +0300

    [integrate] integration regime suggere

--- application/views/templates/navbar.php
+++ application/views/templates/navbar.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
           <a class="nav-link <?php if($metadata['active'] == 'Accueil') { echo( 'active-link'); } ?>" aria-current="page" href="#">Accueil</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link <?php if($metadata['active'] == 'Mes Objectifs') { echo( 'active-link'); } ?> " href="#">Mes Objectifs</a>
+          <a class="nav-link <?php if($metadata['active'] == 'Mes Objectifs') { echo( 'active-link'); } ?> " href="<?= base_url() ?>index.php/mesobjectifs">Mes Objectifs</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" <?php if($metadata['active'] == 'Profil') { echo( 'active-link'); } ?> " href="#">Profil</a>
