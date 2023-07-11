ff998b72796d2c6e2cd566c5162df66ae4a3e0b1
MirijaMarc <mirijarazafimbelo30@gmail.com>
2023-07-11 17:10:50 +0300

    [integration] fix card profil

--- application/views/pages/profil.php
+++ application/views/pages/profil.php
@@ -1,7 +1,7 @@
 <div class="content">
     <div class="transactions">
         <div class="wallet-card-container">
-            <div class="card wallet-card">
+            <div class="card wallet-card border border-success">
                 <div class="card-body">
                     <h5 class="card-title">Solde actuelle</h5>
                     <h1 class="card-text"><?= format_number($solde) ?></h1>
