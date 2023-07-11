6eebcb918007000c9897b177ff146bf3fe860333
Mialy05 <mialisoamurielle@gmail.com>
2023-07-11 17:26:35 +0300

    final

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -21,44 +21,6 @@
                         <span>Régimes</span>
                     </a>
                 </div>
-                <!-- <div class="list-group list-group-flush mx-3">
-                    <a class="list-group-item list-group-item-action py-2 ripple" aria-current="true" data-mdb-toggle="collapse" href="#collapseExample1" aria-expanded="true" aria-controls="collapseExample1">
-                        <i class="fas fa-tachometer-alt fa-fw me-3"></i><span>Expanded menu</span>
-                    </a>
-                    <ul id="collapseExample1" class="collapse show list-group list-group-flush">
-                        <li class="list-group-item py-1">
-                            <a href="" class="text-reset">Link</a>
-                        </li>
-                        <li class="list-group-item py-1">
-                            <a href="" class="text-reset">Link</a>
-                        </li>
-                        <li class="list-group-item py-1">
-                            <a href="" class="text-reset">Link</a>
-                        </li>
-                        <li class="list-group-item py-1">
-                            <a href="" class="text-reset">Link</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="list-group list-group-flush mx-3">
-                    <a class="list-group-item list-group-item-action py-2 ripple" aria-current="true" data-mdb-toggle="collapse" href="#collapseExample2" aria-expanded="true" aria-controls="collapseExample2">
-                        <i class="fas fa-chart-area fa-fw me-3"></i><span>Collapsed menu</span>
-                    </a>
-                    <ul id="collapseExample2" class="collapse list-group list-group-flush">
-                        <li class="list-group-item py-1">
-                            <a href="" class="text-reset">Link</a>
-                        </li>
-                        <li class="list-group-item py-1">
-                            <a href="" class="text-reset">Link</a>
-                        </li>
-                        <li class="list-group-item py-1">
-                            <a href="" class="text-reset">Link</a>
-                        </li>
-                        <li class="list-group-item py-1">
-                            <a href="" class="text-reset">Link</a>
-                        </li>
-                    </ul>
-                </div>
                 <div class="list-group list-group-flush mx-3">
                     <?= anchor("admin/deconnexion", "<i class='fas fa-sign-out fa-fw me-3'></i><span>Deconnexion</span>", ['class' => "list-group-item list-group-item-action py-2 ripple"]) ?>    
                 </div>
