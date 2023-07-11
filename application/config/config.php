5a34d54ea28f0f5c95b2a7fe5b6c0eee289f4e68
Nante <nantemino15@gmail.com>
2023-07-11 17:02:28 +0300

    [deploy] env vars

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/web-s4/';
+$config['base_url'] = 'https://nante-it.alwaysdata.net/web-s4/';
 
 /*
 |--------------------------------------------------------------------------
